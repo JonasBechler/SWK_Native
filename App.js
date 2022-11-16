5ac8412e5746e3fcc51c33f6d9d97e895b030f4c
Jonas Bechler <jonasbechler@gmail.com>
2022-11-17 00:02:32 +0100

    KN webview

--- App.js
+++ App.js
@@ -1,136 +1,155 @@
 import { StyleSheet, View, ImageBackground } from 'react-native';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import ServiceWidget from './ServiceWidgets/ServiceWidget';
 import KonstanzLogin from './ServiceWidgets/KonstanzLogin';
 
 import WebViewer from './WebViewer/WebViewer';
+import WebViewerKnLogin from './WebViewer/WebViewerKnLogin';
 
 import config from './config.json';
 
 
 export default function App() {
+	const [showService, setShowService] = useState(false);
+	const [showKnLogin, setShowKnLogin] = useState(true);
 
-  const [webviewerUrl, setWebviewerUrl] = useState("");
+	const [webviewerUrl, setWebviewerUrl] = useState("");
 
-  
-  const openLink = (link) => {
-    setWebviewerUrl(link);
-  }
 
-  const closeLink = () => {
-    setWebviewerUrl("");
-  }
+	const openLinkService = (link) => {
+		setShowService(true);
+		setWebviewerUrl(link);
+	}
 
-  return (
-    <>
+	const openLinkKonstanz = (link) => {
+		setShowKnLogin(true);
+		setWebviewerUrl(link);
+	}
 
-      {(webviewerUrl === "") ? (
-          <>
-            {/* Show MeinKonstanz screenshot */}
-            <ImageBackground source={require('./BackgroundMeinKonstanz.png')} resizeMode='center' style={styles.BackgroundImage}>
+	const closeLink = () => {
+		setShowService(false);
+		setShowKnLogin(false);
+		//setWebviewerUrl("");
+	}
 
-              {/* Repaint top pixels */}
-              <View style={styles.Top}></View>
+	const get_content = () => {
+		if (showService){
+			return <WebViewer webviewerUrl={webviewerUrl} closeLink={closeLink}></WebViewer>
+		}
 
-              {/* Adding Services */}
-              <ServiceWidget 
-                name={serviceData.Handyticket.name} 
-                icon={serviceData.Handyticket.icon} 
-                link={serviceData.Handyticket.link} 
-                style={styles.Handyticket}
-                openLink={openLink}>
-              </ServiceWidget>
-              
-              <ServiceWidget 
-                name={serviceData.Konrad.name} 
-                icon={serviceData.Konrad.icon} 
-                link={serviceData.Konrad.link} 
-                style={styles.Konrad}
-                openLink={openLink}>
-              </ServiceWidget>
+		else return <>
+			{/* Show MeinKonstanz screenshot */}
+			<ImageBackground source={require('./BackgroundMeinKonstanz.png')} resizeMode='center' style={styles.BackgroundImage}>
 
-              {/* Adding Konstanz Login */}
-              <KonstanzLogin 
-                name={serviceData.KonstanzLogin.name} 
-                icon={serviceData.KonstanzLogin.icon} 
-                link={serviceData.KonstanzLogin.link} 
-                style={styles.KonstanzLogin}
-                openLink={openLink}>
-              </KonstanzLogin>
+				{/* Repaint top pixels */}
+				<View style={styles.Top}></View>
 
-            </ImageBackground>
-          </>
-      ) : (
+				{/* Adding Services */}
+				<ServiceWidget
+					name={serviceData.Handyticket.name}
+					icon={serviceData.Handyticket.icon}
+					link={serviceData.Handyticket.link}
+					style={styles.Handyticket}
+					openLink={openLinkService}>
+				</ServiceWidget>
 
-        
-          <WebViewer webviewerUrl={webviewerUrl} closeLink={closeLink}></WebViewer>
-        
+				<ServiceWidget
+					name={serviceData.Konrad.name}
+					icon={serviceData.Konrad.icon}
+					link={serviceData.Konrad.link}
+					style={styles.Konrad}
+					openLink={openLinkService}>
+				</ServiceWidget>
 
-      )}
-      
-      
-    </>
-  );
+				{/* Adding Konstanz Login */}
+				<KonstanzLogin
+					name={serviceData.KonstanzLogin.name}
+					icon={serviceData.KonstanzLogin.icon}
+					link={serviceData.KonstanzLogin.link}
+					style={styles.KonstanzLogin}
+					openLink={openLinkKonstanz}>
+				</KonstanzLogin>
+
+			</ImageBackground>
+			{(showKnLogin)?(
+				<>
+					<WebViewerKnLogin webviewerUrl={webviewerUrl} closeLink={closeLink}></WebViewerKnLogin>
+				</>
+			) : (
+				<>
+				</>
+			)}
+		</>
+	}
+
+	return (
+		<>
+
+			{
+				get_content()
+			}
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  
-  BackgroundImage: {
 
-    flex: 1,
+	BackgroundImage: {
 
-  },
+		flex: 1,
 
-  Top: {
+	},
 
-    height: 20, 
-    backgroundColor: '#CD2C29'
+	Top: {
 
-  },
+		height: 20,
+		backgroundColor: '#CD2C29'
 
-  Handyticket:{
+	},
 
-    top:33, 
-    left:252,
-    width:106,
-    height:222,
-    
-  },
+	Handyticket: {
 
-  Konrad: {
+		top: 33,
+		left: 252,
+		width: 106,
+		height: 222,
 
-    top:500, 
-    left:17,
-    width:106,
-    height:106,
-    
-  },
+	},
 
-  KonstanzLogin: {
+	Konrad: {
 
-    bottom:0,
-    alignSelf:'center',
-    width:80, 
-    height:55, 
-    
-  }
+		top: 500,
+		left: 17,
+		width: 106,
+		height: 106,
+
+	},
+
+	KonstanzLogin: {
+
+		bottom: 0,
+		alignSelf: 'center',
+		width: 80,
+		height: 55,
+
+	}
 });
 
 const serviceData = {
-  Handyticket: {
-    name: "Handyticket",
-    icon: require('./ServiceWidgets/Icons/Handyticket.png'),
-    link: `${config.device_ip}:${config.handyticket_port_react}`,
-  },
-  Konrad: {
-    name: "Konrad",
-    icon: require('./ServiceWidgets/Icons/Bicycle.png'),
-    link: `${config.device_ip}:${config.konrad.port_react}`
-  },
-  KonstanzLogin: {
-    name: "KN-Login",
-    icon: require('./ServiceWidgets/Icons/KonstanzLoginLogo.jpeg'),
-    link: `${config.device_ip}:${config.login.port_react}`,
-  }
+	Handyticket: {
+		name: "Handyticket",
+		icon: require('./ServiceWidgets/Icons/Handyticket.png'),
+		link: `${config.device_ip}:${config.handyticket.port_react}`,
+	},
+	Konrad: {
+		name: "Konrad",
+		icon: require('./ServiceWidgets/Icons/Bicycle.png'),
+		link: `${config.device_ip}:${config.konrad.port_react}`
+	},
+	KonstanzLogin: {
+		name: "KN-Login",
+		icon: require('./ServiceWidgets/Icons/KonstanzLoginLogo.jpeg'),
+		link: `${config.device_ip}:${config.login.port_react}`,
+	}
 }
---
+++ WebViewer/WebViewerKnLogin.js
@@ -0,0 +1,37 @@
+import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { WebView } from 'react-native-webview'; 
+
+
+export default function WebViewerKnLogin( {webviewerUrl, closeLink} ) {
+
+    function closeWebviewerHandler () {
+        closeLink()
+    }
+    return (
+            
+            
+        <TouchableOpacity activeOpacity={1} style={{position: "absolute", top: 0, left: 0, bottom: 0, right: 0}} onPress={() => closeWebviewerHandler()}>
+
+            <TouchableOpacity activeOpacity={1} style={{position: "absolute", top: 50, left: 30, bottom: 30, right: 30, backgroundColor:"red", borderRadius:15}} onPress={() => {}}>
+                <View style={{margin: 10, flexGrow:1, borderRadius:15}} >
+                    <WebView 
+                        originWhitelist={['*']} 
+                        source={{url: webviewerUrl}}
+                        ></WebView>
+
+                </View>
+
+            </TouchableOpacity>
+
+        </TouchableOpacity>
+    )
+}
+const styles = StyleSheet.create({
+    Image:{
+        width:40,
+        height:40,
+        resizeMode: "contain",
+    
+      },
+})
